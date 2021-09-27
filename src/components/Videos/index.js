ab0080bb19affbf767ff5a7febfd8294b407e57c
Adrien <adrienlcp@gmail.com>
2021-09-27 15:40:32 +0200

    responsive

--- src/components/Videos/index.js
+++ src/components/Videos/index.js
@@ -12,11 +12,13 @@ const Videos = () => {
         videos.map(video => (
           <iframe
             className="videos-item"
+            title={video.description}
+            key={video.id}
             width="560"
             height="315"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
             src={video.link}>
           </iframe>
         ))
